b6d2bc3e6b8b8de341118c98c064bc4ce5afd55d
MUSEMBI BRIAN KITHUSI <112511014+kithusibrian@users.noreply.github.com>
2023-10-11 12:04:11 +0300

    Last Commit i swear

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,18 +1,44 @@
 import React from "react";
-import AcUnitIcon from "@mui/icons-material/AcUnit";
+import "./Footer.css";
 
-const Footer = () => {
+import { SocialIcon } from "react-social-icons";
+
+function Footer() {
   return (
-    <>
-      <footer>
-        <div class="social-icons">
-          <h1>Created by KithusiBrian04</h1>
-          <h2>React.js x WeatherAPI</h2>
-          <AcUnitIcon />
-        </div>
-      </footer>
-    </>
+    <div className="footer">
+      <div className="socialMedi">
+        <SocialIcon
+          url="https://twitter.com/StreetPresenter"
+          className="mr-4"
+          target="_blank"
+          fgColor="#fff"
+          style={{ height: 35, width: 35 }}
+        />
+        <SocialIcon
+          url="https://www.instagram.com/__.sensei._.scott.___/"
+          className="mr-4"
+          target="_blank"
+          fgColor="#fff"
+          style={{ height: 35, width: 35 }}
+        />
+        <SocialIcon
+          url="https://web.facebook.com/BrianKithusi/"
+          className="mr-4"
+          target="_blank"
+          fgColor="#fff"
+          style={{ height: 35, width: 35 }}
+        />
+        <SocialIcon
+          url="https://www.linkedin.com/in/brian-kithusi-9a2a42198/"
+          className="mr-4"
+          target="_blank"
+          fgColor="#fff"
+          style={{ height: 35, width: 35 }}
+        />
+      </div>
+      <p>&copy; 2023 Brian Kithusi Tech</p>
+    </div>
   );
-};
+}
 
 export default Footer;
